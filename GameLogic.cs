66ee1afbaba61d32e0e81bb0b8960d549ec7b0ec
Nikita Sychev <nikitasychev@inbox.ru>
2023-08-27 15:58:07 +0500

    Done ( но с компоновкой не могу разобраться )

---
+++ GameLogic.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject_W_F
+{
+    internal class GameLogic
+    {
+
+        int size = 4;
+        int[,] map;
+        int spaseX;
+        int spaseY;
+        static Random random = new Random();
+
+        public GameLogic()
+        {
+            map = new int[size, size];
+        }
+        public void Start()
+        {
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    map[x, y] = ConvertCoordinatesToPosition(x, y) +1;
+            spaseX = size - 1;
+            spaseY = size - 1;
+            map[spaseX, spaseY] = 0;
+        }
+        private int ConvertCoordinatesToPosition(int x,int y)
+        {
+            if (x < 0) x = 0;
+            if (x > size - 1) x = size - 1;
+            if(y < 0) y = 0;
+            if(y> size - 1) y = size - 1;
+            return y * size + x;
+        }
+        private void ConvertPositionToCoords(int position, out int x, out int y)
+        {
+            if(position < 0) position = 0;  
+            if(position > size * size - 1) position = size*size - 1;
+            x = position% size;
+            y = position/size;
+        }
+        public void ShiftRandom()
+        {
+            int number = random.Next(0, 4);
+            int x = spaseX;
+            int y = spaseY;
+            switch (number)
+            {
+                case 0:x--;break;
+                case 1:x++;break;
+                case 2:y--;break;
+                case 3:y++;break;
+            }
+            Shift(ConvertCoordinatesToPosition(x, y));
+        }
+        public void Shift(int position)
+        {
+            int x, y;
+            ConvertPositionToCoords(position, out x, out y);
+            if (Math.Abs(spaseX - x) + Math.Abs(spaseY - y) != 1)
+                return;
+            map[spaseX, spaseY] = map[x, y];
+            map[x, y] = 0;
+            spaseX = x;
+            spaseY = y;
+
+
+        }
+        public int GetNumber (int position)
+        {
+            int x, y;
+            ConvertPositionToCoords(position, out x, out y);
+            if (x < 0 || x >= size) return 0;
+            if (y < 0 || y >= size) return 0;
+            return map[x, y];
+
+        }
+
+        public bool CheckNumbers()
+        {
+            if(!(spaseX==size-1 && spaseY==size-1)) return false;
+            for(int x = 0; x < size; x++)
+                for(int y = 0; y < size; y++)
+                {
+                    if (!(x == size - 1 && y == size - 1))
+                        if (map[x, y] != ConvertCoordinatesToPosition(x, y) + 1) return false;
+                }
+            return true;
+        }
+
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,10 +19,82 @@ namespace FirstProject_W_F
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
+
+
     {
+        GameLogic game;
+
         public MainWindow()
         {
             InitializeComponent();
+            game = new GameLogic();
+
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            game.Start();
+            for (int i = 0; i < 16; i++)
+            {
+                game.ShiftRandom();
+              // RefreshButtons();
+            }
+        }
+
+        private void RefreshButtons()
+        {
+            for (int position = 0; position < 16; position++)
+            {
+
+
+                int number = game.GetNumber(position);
+                button(position).Content = number.ToString();
+                if (number > 0)
+                    button(position).Visibility = Visibility.Visible;
+                else if (number == 0)
+                    button(position).Visibility = Visibility.Collapsed;
+
+            }
+
+        }
+        private Button button(int position)
+        {
+            switch (position)
+            {
+                case 0: return Button0;
+                case 1: return Button1;
+                case 2: return Button2;
+                case 3: return Button3;
+                case 4: return Button4;
+                case 5: return Button5;
+                case 6: return Button6;
+                case 7: return Button7;
+                case 8: return Button8;
+
+                case 9: return Button9;
+                case 10: return Button10;
+                case 11: return Button11;
+                case 12: return Button12;
+                case 13: return Button13;
+                case 14: return Button14;
+
+                case 15: return Button15;
+                default: return Button0;
+
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int position = Convert.ToInt16(((Button)sender).Tag);
+            game.Shift(position);
+            RefreshButtons();
+            if (game.CheckNumbers())
+                MessageBox.Show("You win");
+           // StartGame();
         }
     }
 }
